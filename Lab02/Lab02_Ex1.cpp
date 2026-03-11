c0c71388cd9b511a2d6819e65a17a31b2216726d
李品融 <t113310217@ntut.org.tw>
2026-03-11 21:45:55 +0800

    Update Lab02_Ex1.cpp

--- Lab02/Lab02_Ex1.cpp
+++ Lab02/Lab02_Ex1.cpp
@@ -1,19 +1,33 @@
-int findMax(const vector<int>& arr) 
-{ 
- // TODO: Add counts for initialization 
- int max = arr[0]; //一次陣列存取+一次賦值，共兩步 
-  
- // TODO: Add counts for loop operations 
- for (int i = 1; i < arr.size(); i++) { //for迴圈初始化賦值，共一步 
- // TODO: Add counts for comparison and assignment  if (arr[i] > max) { 
- max = arr[i]; 
- } 
- } 
- // TODO: Add counts for return 
- return max; 
-}
+int findMax(const vector<int>& arr) {
+// TODO: Add counts for initialization
 
-// Total operations:
-// 3 (initial) + 6 * (n-1) (loop) + 1 (exit) + 1 (return)
-// = 6n - 1 operations
+ int stepCount = 0 //假設步數
+  
+ int max = arr[0];
+ stepCount++; //陣列存取arr[0]與賦值給max，1步 
+// TODO: Add counts for loop operations
+
+ stepCount++; //for迴圈初始化(i=1)，1步 
+for (int i = 1; i < arr.size(); i++) {
+// TODO: Add counts for comparison and assignment
+
+ stepCount++; //比較i<arr.size()，1步
+ 
+ stepCount++; //比較arr[i]>max，1步 
+ if (arr[i] > max) {
+  max = arr[i];
+  stepCount++; //賦值運算(將新最大值存入max)，1步 
+ }
+ stepCount++; //增量運算(i++)，1步 
+}
+// TODO: Add counts for return
+
+stepCount++; //for迴圈結束時最後一次失敗的比較，1步 
+stepCount++; //return動作，1步 
+return max;
+}
+// Total operations(總運算次數):
+// 2 (初始化) + 4 * (n-1) (for迴圈內的運算) + 1 (跳出迴圈) + 1 (return回傳結果)
+// = 4 + 4(n-1) operations
+// = 4n operations
 // Therefore, O(n) complexity
