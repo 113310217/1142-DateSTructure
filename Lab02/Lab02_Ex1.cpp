e1b8bc7b506d0e9bc29b876caa6be0c876acd56c
李品融 <t113310217@ntut.org.tw>
2026-03-11 15:55:29 +0800

    Update Lab02_Ex1.cpp

--- Lab02/Lab02_Ex1.cpp
+++ Lab02/Lab02_Ex1.cpp
@@ -1,1 +1,19 @@
+int findMax(const vector<int>& arr) 
+{ 
+ // TODO: Add counts for initialization 
+ int max = arr[0]; //一次陣列存取+一次賦值，共兩步 
+  
+ // TODO: Add counts for loop operations 
+ for (int i = 1; i < arr.size(); i++) { //for迴圈初始化賦值，共一步 
+ // TODO: Add counts for comparison and assignment  if (arr[i] > max) { 
+ max = arr[i]; 
+ } 
+ } 
+ // TODO: Add counts for return 
+ return max; 
+}
 
+// Total operations:
+// 3 (initial) + 6 * (n-1) (loop) + 1 (exit) + 1 (return)
+// = 6n - 1 operations
+// Therefore, O(n) complexity
